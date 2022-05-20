5431238db753c793012875304a01dc02c70d45ce
Fabio Mucciante <fabio.mucciante@gmail.com>
2022-05-20 15:51:15 +0200

    Added the genre_s tag.

--- lib/filter_rename/filetype/mp3_filename.rb
+++ lib/filter_rename/filetype/mp3_filename.rb
@@ -16,9 +16,9 @@ module FilterRename
 
     def ==(dest)
       super &&
-      ([ @title, @artist, @album, @track, @comments, @year, @genre ] ==
+      ([ @title, @artist, @album, @track, @comments, @year, @genre, @genre_s ] ==
        [ dest.get_string(:title), dest.get_string(:artist), dest.get_string(:album), dest.get_string(:track),
-         dest.get_string(:comments), dest.get_string(:year), dest.get_string(:genre) ])
+         dest.get_string(:comments), dest.get_string(:year), dest.get_string(:genre), dest.get_string(:genre_s) ])
     end
 
     def rename!(dest)
@@ -27,7 +27,7 @@ module FilterRename
       Mp3Info.open(full_filename) do |mp3|
         old_data.merge!({ title: mp3.tag.title, artist: mp3.tag.artist, album: mp3.tag.album,
                           tracknum: mp3.tag.tracknum, comments: mp3.tag.comments, year: mp3.tag.year,
-                          genre_s: mp3.tag.genre_s })
+                          genre: mp3.tag.genre, genre_s: mp3.tag.genre_s })
 
         mp3.tag.title = dest.get_string(:title)
         mp3.tag.artist = dest.get_string(:artist)
@@ -35,8 +35,8 @@ module FilterRename
         mp3.tag.tracknum = dest.get_string(:track)
         mp3.tag.comments = dest.get_string(:comments).to_s
         mp3.tag.year = dest.get_string(:year)
-        mp3.tag.genre_s = dest.get_string(:genre)
-
+        mp3.tag.genre = dest.get_string(:genre).to_i % 256 
+        mp3.tag.genre_s = dest.get_string(:genre_s)
       end
 
       load_mp3_data(full_filename)
@@ -53,6 +53,7 @@ module FilterRename
        Comments: #{Differ.diff_by_word(dest.get_string(:comments).to_s, @comments.to_s)}
        Year:     #{Differ.diff_by_word(dest.get_string(:year).to_s, @year.to_s)}
        Genre:    #{Differ.diff_by_word(dest.get_string(:genre).to_s, @genre.to_s)}
+       GenreS:   #{Differ.diff_by_word(dest.get_string(:genre_s).to_s, @genre_s.to_s)}
        "
     end
 
@@ -67,7 +68,8 @@ module FilterRename
       @track = mp3info.tag.tracknum.to_i
       @comments = mp3info.tag.comments.to_s
       @year = mp3info.tag.year.to_i
-      @genre = mp3info.tag.genre_s.to_s
+      @genre = mp3info.tag.genre.to_i
+      @genre_s =  mp3info.tag.genre_s.to_s
 
       # read only stuff
       @vbr = (mp3info.tag.vbr ? 'vbr' : '')
