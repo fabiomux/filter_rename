9fe5380baffa80a9fa157ac6f560a2f68172051a
Fabio Mucciante <fabio.mucciante@gmail.com>
2025-06-26 18:10:58 +0200

    Removed the -u switch to the --virtual option.

--- lib/filter_rename/cli.rb
+++ lib/filter_rename/cli.rb
@@ -75,7 +75,7 @@ module FilterRename
           end
         end
 
-        opt.on("-u", "--virtual", "Accept a list of file not present in the current drive") do |_c|
+        opt.on("--virtual", "Accept a list of file not present in the local drives") do |_c|
           options.global.store(:mimemagic, false)
           options.global.store(:essential_tags, true)
           options.global.store(:check_source, false)
